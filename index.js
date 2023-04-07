3bf424040465971bad951955de770efb9418416a
Oscar Dominguez <dominguez.celada@gmail.com>
2023-04-07 21:04:08 +0200

    feat(issue-created): test probot app
    https://github.com/probot/probot/issues/1795#issue-1569855955

--- index.js
+++ index.js
@@ -1,64 +1,26 @@
 /**
+ * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
-  app.log("Yay! The app was loaded!");
+  // Your code here
+  app.log.info("Yay, the app was loaded!");
 
-  async function findOrCreateStargazersIssue(context) {
-    const OWNER = context.payload.repository.owner.login;
-    const REPO = context.payload.repository.name;
-    const LABEL = "stargazers";
-
-    const { data: issues } = await context.octokit.issues.listForRepo({
-      repo: REPO,
-      owner: OWNER,
-      state: "open",
-    });
-
-    const stargazersIssue = issues.find((issue) =>
-      issue.labels.some((label) => label.name === LABEL)
+  app.on("issues.opened", async (context) => {
+    return context.octokit.issues.createComment(
+      context.issue({ body: "Thanks for opening this issue!" })
     );
-
-    if (stargazersIssue) {
-      return stargazersIssue.number;
-    } else {
-      const { data: issue } = await context.octokit.issues.create({
-        repo: REPO,
-        owner: OWNER,
-        body: "This issue tracks the stargazers and the runaway stargazers of this repo",
-        labels: [LABEL],
-        title: "Issue to track stargazers",
-      });
-      return issue.number;
-    }
-  }
-
-  app.on(["star.created", "star.deleted"], async (context) => {
-    const issueNumber = await findOrCreateStargazersIssue(context);
-
-    const OWNER = context.payload.repository.owner.login;
-    const REPO = context.payload.repository.name;
-    const STARGAZERS = context.payload.repository.stargazers_count;
-    const {
-      data: { login: USER },
-    } = await context.octokit.users.getByUsername({
-      username: context.payload.sender.login,
-    });
-
-    const commentBody =
-      context.name === "star" && context.payload.action === "created"
-        ? `Thank you so much for starring this repo, ${USER} :pray:, this means a lot! \n ${REPO} has ${
-            STARGAZERS > 1 ? `${STARGAZERS}s` : STARGAZERS
-          } now`
-        : `${USER} just unstarred this repository :cry: :cry: \n ${REPO} has ${
-            STARGAZERS > 1 ? `${STARGAZERS}s` : STARGAZERS
-          } now`;
-
-    return context.octokit.issues.createComment({
-      owner: OWNER,
-      repo: REPO,
-      issue_number: issueNumber,
-      body: commentBody,
-    });
   });
+
+  app.on("issue_comment.created", async (context) => {
+    return context.octokit.issues.createComment(
+      context.issue({ body: "Thanks for commenting on this issue!" })
+    );
+  });
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
 };
